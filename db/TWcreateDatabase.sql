d373b9decd583d173de3e981e6ef777a88e17078
Botti Bao <botti.bao@studio.unibo.it>
2024-11-30 14:24:32 +0100

    Add Recensione, notifica entity

--- db/TWcreateDatabase.sql
+++ db/TWcreateDatabase.sql
@@ -3,8 +3,8 @@
 -- *--------------------------------------------
 -- * DB-MAIN version: 11.0.2              
 -- * Generator date: Sep 14 2021              
--- * Generation date: Thu Nov 28 12:11:32 2024 
--- * LUN file: C:\Users\Franco\Downloads\ER.lun 
+-- * Generation date: Sat Nov 30 14:09:08 2024 
+-- * LUN file: C:\Users\Franco\OneDrive\Desktop\Scuola\Elaborato_WEB_2024\db\ER.lun 
 -- * Schema: LogicaFinale/1 
 -- ********************************************* 
 
@@ -12,60 +12,76 @@
 -- Database Section
 -- ________________ 
 
-create database TWDatabase;
-use TWDatabase;
+create database twdatabase;
+use twdatabase;
 
 
 -- Tables Section
 -- _____________ 
 
-create table if not exists CARRELLO (
+create table CARRELLO (
      codUtente int not null,
      codProdotto int not null,
      quantita int not null,
      constraint SID_CARRELLO_ID unique (codUtente, codProdotto));
 
-create table if not exists CATEGORIE (
+create table CATEGORIA (
      codCategoria int not null auto_increment,
      nome varchar(20) not null,
-     descrizione varchar(500) not null,
-     constraint ID_CATEGORIE_ID primary key (codCategoria));
+     descrizione varchar(300) not null,
+     constraint ID_CATEGORIA_ID primary key (codCategoria));
 
-create table if not exists DETTAGLIO_ORDINE (
+create table DETTAGLIO_ORDINE (
      codOrdine int not null,
      codProdotto int not null,
      quantita int not null,
      constraint ID_DETTAGLIO_ORDINE_ID primary key (codProdotto, codOrdine));
 
-create table if not exists ORDINE (
+create table NOTIFICA (
+     codNotifica int not null auto_increment,
+     messaggio varchar(200) not null,
+     tipoNotifica varchar(20) not null,
+     letto char not null,
+     dataNotifica datetime not null,
      codUtente int not null,
+     constraint ID_NOTIFICA_ID primary key (codNotifica));
+
+create table RECENSIONE (
+     codUtente int not null,
+     codProdotto int not null,
+     votoRecensione int not null,
+     commento varchar(500) not null,
+     dataRecensione datetime not null,
+     constraint ID_RECENSIONE_ID primary key (codUtente, codProdotto));
+
+create table ORDINE (
      codOrdine int not null auto_increment,
      dataOrdine date not null,
      statoOrdine varchar(20) not null,
-     totale double(8,2) not null,
+     totale int not null,
      pagato char not null,
+     codUtente int not null,
      constraint ID_ORDINE_ID primary key (codOrdine));
 
-create table if not exists PRODOTTO (
+create table PRODOTTO (
      codProdotto int not null auto_increment,
-     nome varchar(50) not null,
-     descrizione varchar(200) not null,
+     nome varchar(30) not null,
+     descrizione varchar(300) not null,
      quantitaResidua int not null,
-     immagine varchar(100) not null,
-     prezzo double(8,2) not null,
+     prezzo int not null,
+     immagine varchar(30) not null,
      codCategoria int not null,
      constraint ID_PRODOTTO_ID primary key (codProdotto));
 
-create table if not exists UTENTE (
+create table UTENTE (
      codUtente int not null auto_increment,
-     nomeUtente varchar(50) not null,
-     password varchar(50) not null,
-     email varchar(50) not null,
+     nomeUtente varchar(20) not null,
+     password varchar(20) not null,
+     email varchar(30) not null,
      privilegi char not null,
-     indirizzo varchar(50) not null,
+     indirizzo varchar(30) not null,
      citta varchar(20) not null,
-     constraint ID_UTENTE_ID primary key (codUtente),
-     constraint ID_UTENTE_1 unique (email));
+     constraint ID_UTENTE_ID primary key (codUtente));
 
 
 -- Constraints Section
@@ -79,11 +95,6 @@ alter table CARRELLO add constraint FKcontiene_FK
      foreign key (codProdotto)
      references PRODOTTO (codProdotto);
 
--- Not implemented
--- alter table CATEGORIE add constraint ID_CATEGORIE_CHK
---     check(exists(select * from PRODOTTO
---                  where PRODOTTO.codCategoria = codCategoria)); 
-
 alter table DETTAGLIO_ORDINE add constraint FKDET_PRO
      foreign key (codProdotto)
      references PRODOTTO (codProdotto);
@@ -92,13 +103,25 @@ alter table DETTAGLIO_ORDINE add constraint FKDET_ORD_FK
      foreign key (codOrdine)
      references ORDINE (codOrdine);
 
-alter table ORDINE add constraint FKeffetuare
+alter table NOTIFICA add constraint FKpossiede__FK
+     foreign key (codUtente)
+     references UTENTE (codUtente);
+
+alter table RECENSIONE add constraint FKriceve_FK
+     foreign key (codProdotto)
+     references PRODOTTO (codProdotto);
+
+alter table RECENSIONE add constraint FKscrive
+     foreign key (codUtente)
+     references UTENTE (codUtente);
+
+alter table ORDINE add constraint FKeffetuare_FK
      foreign key (codUtente)
      references UTENTE (codUtente);
 
 alter table PRODOTTO add constraint FKfa_parte_FK
      foreign key (codCategoria)
-     references CATEGORIE (codCategoria);
+     references CATEGORIA (codCategoria);
 
 
 -- Index Section
@@ -110,8 +133,8 @@ create unique index SID_CARRELLO_IND
 create index FKcontiene_IND
      on CARRELLO (codProdotto);
 
-create unique index ID_CATEGORIE_IND
-     on CATEGORIE (codCategoria);
+create unique index ID_CATEGORIA_IND
+     on CATEGORIA (codCategoria);
 
 create unique index ID_DETTAGLIO_ORDINE_IND
      on DETTAGLIO_ORDINE (codProdotto, codOrdine);
@@ -119,8 +142,23 @@ create unique index ID_DETTAGLIO_ORDINE_IND
 create index FKDET_ORD_IND
      on DETTAGLIO_ORDINE (codOrdine);
 
+create unique index ID_NOTIFICA_IND
+     on NOTIFICA (codNotifica);
+
+create index FKpossiede__IND
+     on NOTIFICA (codUtente);
+
+create unique index ID_RECENSIONE_IND
+     on RECENSIONE (codUtente, codProdotto);
+
+create index FKriceve_IND
+     on RECENSIONE (codProdotto);
+
 create unique index ID_ORDINE_IND
      on ORDINE (codOrdine);
+
+create index FKeffetuare_IND
+     on ORDINE (codUtente);
 
 create unique index ID_PRODOTTO_IND
      on PRODOTTO (codProdotto);
