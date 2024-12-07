fa8f6ccf0e683e60f86f7775771848572090c85c
Botti Bao <botti.bao@studio.unibo.it>
2024-12-08 00:50:41 +0100

    add review part in singolo-prodotto

--- bootstrap/template/singolo-prodotto.php
+++ bootstrap/template/singolo-prodotto.php
@@ -1,3 +1,4 @@
+<!-- TODO getdata from database -->
 <h2>Nome Prodotto</h2>
 
 <p class="text-body-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec orci sit amet elit
@@ -12,15 +13,15 @@
     <div class="col text-start"><span class="bi bi-star-fill text-custom-lgold"></span> 4.5/5</div>
 </div>
 <!-- quantita -->
-<div class="row align-items-center mb-2">
-    <div class="col-3">
-        <label for="quantita" class="form-label">Quantità</label>
+<div class="row align-items-center mb-3">
+    <div class="col-3 d-flex align-items-center">
+        <label for="quantita" class="form-label mb-0">Quantità</label>
     </div>
     <div class="col">
         <div class="input-group">
-            <button class="btn btn-outline-secondary" type="button" id="decrement">-</button>
-            <input id="quantita" type="number" class="form-control text-center" value="1" min="1">
-            <button class="btn btn-outline-secondary" type="button" id="increment">+</button>
+            <button class="btn btn-outline-secondary button-size" type="button" id="decrement">-</button>
+            <input id="quantita" type="number" class="form-control text-center border-secondary" value="1" min="1">
+            <button class="btn btn-outline-secondary button-size" type="button" id="increment">+</button>
         </div>
     </div>
 </div>
@@ -30,3 +31,28 @@
     <button class="btn btn-custom-lgold">Aggiungi al Carrello</button>
     <button class="btn btn-custom-gold">Compra Subito</button>
 </div>
+
+<hr>
+
+<!-- recensioni TODO da cambiare con foreach ecc..-->
+<section class="mt-2">
+        <h3 class="fw-bold mb-3">Recensioni</h3>
+        <ul>
+            <li class="d-flex align-items-start mb-3">
+                <img src="img/temp.jpg" class="rounded-circle me-3 user-avatar-size" alt="Profilo Utente" />
+                <div>
+                    <strong>Pimpo Bello</strong>
+                    <p class="mb-1"><span class="bi bi-star-fill text-custom-lgold"></span> 4/5</p>
+                    <p class="text-body-secondary mb-0">Bello!</p>
+                </div>
+            </li>
+            <li class="d-flex align-items-start">
+                <img src="img/temp.jpg" class="rounded-circle me-3 user-avatar-size" alt="Profilo Utente" />
+                <div>
+                    <strong>Giulio Brutto</strong>
+                    <p class="mb-1"><span class="bi bi-star-fill text-custom-lgold"></span> 1/5</p>
+                    <p class="text-body-secondary mb-0">Brutto, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quae, esse est debitis velit exercitationem corrupti voluptate magnam consectetur commodi error vel beatae aliquam minima repellat id ipsam perspiciatis mollitia!</p>
+                </div>
+            </li>
+        </ul>
+    </section>
