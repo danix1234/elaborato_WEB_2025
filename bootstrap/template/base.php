c164115cae2d492da03a1041aba596069e62def8
Botti Bao <botti.bao@studio.unibo.it>
2024-12-07 23:52:26 +0100

    mod base.php, add main

--- bootstrap/template/base.php
+++ bootstrap/template/base.php
@@ -8,7 +8,7 @@
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
 
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="./css/style.css" />
 
     <title>Nostro Sito</title> <!-- TODO change title -->
 </head>
@@ -16,11 +16,11 @@
 <body class="bg-white">
     <header class="container-fluid p-0 overflow-hidden bg-custom-blue">
 
-        <div class="row px-1">
-            <div class="col-3 d-flex align-items-center justify-content-center">
-                <a href="#"><img src="img/temp.jpg" class="img-fluid" alt="Logo" /></a><!-- TODO add path to img -->
+        <div class="row d-flex align-items-center justify-content-center px-1">
+            <div class="col-3">
+                <a href="#"><img src="./img/temp.jpg" class="img-fluid" alt="Logo" /></a><!-- TODO add path to img -->
             </div>
-            <div class="col d-flex align-items-center justify-content-center text-white">ciao, accedi</div>
+            <div class="col text-center text-white">ciao, accedi</div>
             <!-- TODO add correct ref -->
             <div class="col-4 d-flex align-items-center">
                 <div class="w-100 d-flex justify-content-around">
@@ -36,7 +36,7 @@
         <!-- search bar -->
         <div class="row mt-2 px-3">
             <div class="input-group">
-                <label for="searchBar" class="visually-hidden">Cerca</label>
+                <label for="searchBar" class="visually-hidden form-label">Cerca</label>
                 <input id="searchBar" type="search" class="form-control rounded-start rm-border" placeholder="Cerca" />
                 <a href="#" class="input-group-text bg-custom-lgold rm-border" title="cerca">
                     <span class="bi bi-search"></span> </a>
@@ -45,7 +45,7 @@
     </header>
 
     <!-- category -->
-    <div class="row px-2 overflow-auto bg-custom-lblue">
+    <!-- <div class="row px-2 overflow-auto bg-custom-lblue">
         <nav class="nav d-flex flex-nowrap w-100">
             <a class="nav-link text-white" href="#">categoria1</a>
             <a class="nav-link text-white" href="#">categoria2</a>
@@ -53,14 +53,14 @@
             <a class="nav-link text-white" href="#">categoria4</a>
             <a class="nav-link text-white" href="#">categoria5</a>
         </nav>
-    </div>
+    </div> -->
 
-    <main>
-        sono il main
-</br>
-</br>
-</br>
-</br>
+    <main class="container mt-2">
+        <?php
+        if (isset($templateParams["nome"])) {
+            require($templateParams["nome"]);
+        }
+        ?>
     </main>
 
     <footer class="text-center bg-custom-blue text-white">
