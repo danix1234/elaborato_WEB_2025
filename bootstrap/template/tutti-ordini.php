11739f9b97fd17dce316788f9977dee7321da2d4
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-12-08 17:29:45 +0100

    added tutti-ordini page

--- bootstrap/template/tutti-ordini.php
+++ bootstrap/template/tutti-ordini.php
@@ -1,11 +1,29 @@
 <!--title of entire page-->
 <header class="row">
     <h1 class="col text-center">
-        Ordine
+        Ordini
     </h1>
 </header>
 
 <div class="container-fluid">
-    <div class="row">
-    </div>
+    <table class="table table-bordered table-striped table-hover table-responsive">
+        <thead>
+            <tr>
+                <th scope="col">Id ordine</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Data</th>
+                <th scope="col">Totale</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php for ($i=0; $i < 5 ; $i++) { ?>
+            <tr>
+                <th scope="row">id ordine</th>
+                <td>questo ordine contiene: 2 laptop, 3 rasoi, 1 banana, 3 kiwi, 4 lattughe, 2 schiaffi in faccia</td>
+                <td>12/05/2024</td>
+                <td>25.99$</td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </div>
