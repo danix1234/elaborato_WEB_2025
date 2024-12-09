06c0985c61afce26efc6229ce9a3cc9a6374b5f3
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-12-09 04:56:15 +0100

    wip: prodotto-admin

--- website/template/prodotto-admin.php
+++ website/template/prodotto-admin.php
@@ -1,11 +1,22 @@
 <!--title of entire page-->
 <header class="row">
     <h1 class="col text-center">
-        Inserisci/Modifica Prodotto
+        Inserisci Prodotto
     </h1>
 </header>
 
 <!--modifica del prodotto-->
-<div class="row">
+<form class="row" action="#" method="">
+    <div class="col-xl-6">
+        <img class="img-fluid" src="img/temp.jpg" alt="">
+        <label class="form-label" for="">Scegli Immagine </label>
+        <input class="form-control" type="file" name="" value="">
+    </div>
+    <div class="col-xl-6">
+        <label class="form-label" for="">Nome</label>
+        <input class="form-control" type="text" name="" value="" />
+        <label class="form-label" for="">Descrizione</label>
+        <textarea class="form-control" cols="50" rows="3"></textarea>
+    </div>
 
-</div>
+</form>
