1bdd85326d69f9a9107fe5dfc904a27c8c1d21f1
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-12-09 02:25:14 +0100

    small fix

---
+++ website/template/prodotto-admin.php
@@ -0,0 +1,11 @@
+<!--title of entire page-->
+<header class="row">
+    <h1 class="col text-center">
+        Inserisci/Modifica Prodotto
+    </h1>
+</header>
+
+<!--modifica del prodotto-->
+<div class="row">
+
+</div>
