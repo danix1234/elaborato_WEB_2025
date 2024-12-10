16490d4072cdfbe59542d204c98a52f842070477
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-12-10 17:21:35 +0100

    wip: carrello

--- website/template/carrelli.php
+++ website/template/carrelli.php
@@ -9,41 +9,55 @@
     </a>
 </header>
 
-<div class="row justify-content-center">
-    <!--prodotti-->
-    <section class="col-md-8">
-        <?php for ($i=0; $i < 5 ; $i++) { ?>
-        <div class="row">
-            <a class="col-md-4" href="#">
-                <img class="img-fluid" src="img/temp.jpg" alt="" />
-            </a>
-            <div class="col-md-8">
-                <div class="row">
-                    <h2>Nome del prodotto</h2>
-                </div>
-                <div class="row">
-                    <p>Descrizione non troppo lunga e non troppo complessa del prodotto.</p>
-                </div>
-                <div class="row justify-content-between">
-                    <div class="col-auto">
-                        <label class="col-auto">Quantità <input type="number" name="id" value="1" min="1" max="100" />
-                        </label>
-                        <button class="col-auto bg-custom-lgold" type="button">Rimuovi</button>
-                    </div>
-                    <span class="col-auto">Prezzo: 19.99$</span>
-                </div>
-            </div>
-        </div>
-        <?php } ?>
-    </section>
+<!--lista dei prodotti-->
+<section class="row mx-1 mx-md-4">
+</section>
 
-    <!--resoconto finale-->
-    <section class="text-center col-md-4">
-        <div class="row">
-            <p>Prezzo totale (n prodotti): 16.69$</p>
-        </div>
-        <div class="row">
-            <button type="button" class="btn bg-custom-lgold" id="buy">Procedi all'acquisto</button>
-        </div>
-    </section>
-</div>
+<!--resoconto-->
+<section class="mx-1 mx-md-4 my-2">
+    <div class="row justify-content-center">
+        <p class="col-auto my-1 align-middle">Prezzo totale: 16.69$</p>
+    </div>
+    <div class="row justify-content-center ">
+        <button type="button" class="col-auto btn bg-custom-lgold bg-custom-gold-hover" id="buy">Acquista</button>
+    </div>
+</section>
+
+<!--<div class="row justify-content-center">-->
+<!--    <!--prodotti-->
+<!--    <section class="col-md-8">-->
+<!--        <?php for ($i=0; $i < 5 ; $i++) { ?>-->
+<!--        <div class="row">-->
+<!--            <a class="col-md-4" href="#">-->
+<!--                <img class="img-fluid" src="img/temp.jpg" alt="" />-->
+<!--            </a>-->
+<!--            <div class="col-md-8">-->
+<!--                <div class="row">-->
+<!--                    <h2>Nome del prodotto</h2>-->
+<!--                </div>-->
+<!--                <div class="row">-->
+<!--                    <p>Descrizione non troppo lunga e non troppo complessa del prodotto.</p>-->
+<!--                </div>-->
+<!--                <div class="row justify-content-between">-->
+<!--                    <div class="col-auto">-->
+<!--                        <label class="col-auto">Quantità <input type="number" name="id" value="1" min="1" max="100" />-->
+<!--                        </label>-->
+<!--                        <button class="col-auto bg-custom-lgold" type="button">Rimuovi</button>-->
+<!--                    </div>-->
+<!--                    <span class="col-auto">Prezzo: 19.99$</span>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <?php } ?>-->
+<!--    </section>-->
+<!---->
+<!--    <!--resoconto finale-->
+<!--    <section class="text-center col-md-4">-->
+<!--        <div class="row">-->
+<!--            <p>Prezzo totale (n prodotti): 16.69$</p>-->
+<!--        </div>-->
+<!--        <div class="row">-->
+<!--            <button type="button" class="btn bg-custom-lgold" id="buy">Procedi all'acquisto</button>-->
+<!--        </div>-->
+<!--    </section>-->
+<!--</div>-->
