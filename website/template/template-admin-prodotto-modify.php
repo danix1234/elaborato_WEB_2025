ef9ff5f32396edca48f38623e537527333500c39
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-12-12 21:48:50 +0100

    no required for modify, and all fields have a placeholder

--- website/template/template-admin-prodotto-modify.php
+++ website/template/template-admin-prodotto-modify.php
@@ -12,22 +12,25 @@
             <img class="img-fluid" src="img/temp.jpg" alt="immagine descrittiva del prodotto" />
             <label class="form-label visually-hidden" for="preview">Scegli Immagine </label>
             <input class="form-control image-custom-preview" type="file" accept="image/png, image/jpeg" name="preview"
-                id="preview" required />
+                id="preview" />
         </div>
         <div class="col-md-6 ps-md-3">
             <label class="form-label" for="name">Nome</label>
-            <input class="form-control" type="text" name="name" id="name" required />
+            <input class="form-control" type="text" name="name" id="name" placeholder="current-value" />
             <label class="form-label" for="description">Descrizione</label>
-            <textarea class="form-control" cols="50" rows="3" name="description" id="description" required></textarea>
+            <textarea class="form-control" cols="50" rows="3" name="description" id="description"
+                placeholder="current-value"></textarea>
             <label class="form-label" for="price">Prezzo unitario</label>
             <div class="input-group">
-                <input class="form-control button-custom-float" type="text" name="price" id="price" required />
+                <input class="form-control button-custom-float" type="text" name="price" id="price"
+                    placeholder="current-value" />
                 <span class="input-group-text">€</span>
             </div>
             <label class="form-label" for="quantity">Quantità residua</label>
             <div class="input-group">
                 <button tabindex="-1" class="input-group-text font-monospace" type="button" id="decrement">-</button>
-                <input class="form-control button-custom-quantity" type="text" name="quantity" id="quantity" required />
+                <input class="form-control button-custom-quantity" type="text" name="quantity" id="quantity"
+                    placeholder="current-value" />
                 <button tabindex="-1" class="input-group-text font-monospace" type="button" id="increment">+</button>
             </div>
         </div>
