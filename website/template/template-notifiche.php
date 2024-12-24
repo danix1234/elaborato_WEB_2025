23c3926c70ebf6529809809e1490c95e59b3350e
Giuseppe Marini <giuseppe.marini4@studio.unibo.it>
2024-12-24 13:56:22 +0100

    modify template notifiche

--- website/template/template-notifiche.php
+++ website/template/template-notifiche.php
@@ -1,25 +1,27 @@
- <div class="container text-center">
-     <div class="row row-cols-3 row-cols-md-4 my-4">
+ <div class="container-fluid text-center">
+     <div class="row my-4">
          <div class="col">
-             <button type="button" class="btn" data-bs-toggle="button">Seleziona tutto</button>
+             <button type="button" class="btn btn-custom-gold" data-bs-toggle="button">Seleziona tutto</button>
          </div>
          <div class="col">
-             <button type="button" class="btn" data-bs-toggle="button">Leggi tutto</button>
+             <button type="button" class="btn btn-custom-gold" data-bs-toggle="button">Segna come lette</button>
          </div>
          <div class="col">
-             <button type="button" class="btn" data-bs-toggle="button">Da leggere</button>
-         </div>
-         <div class="col d-none d-md-block">
-             <button type="button" class="btn" data-bs-toggle="button">Già letti</button>
+             <button type="button" class="btn btn-custom-gold" data-bs-toggle="button">Elimina selezionate</button>
          </div>
      </div>
      <?php for ($i = 0; $i < 4; $i++) { ?>
-         <div class="row cols-12 mb-3">
-             <div class="col-2"><a href="#"><img src="img/temp.jpg" class="img-fluid" alt="Logo" /></a></div>
-             <div class="col-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aliquid
+         <div class="row align-items-center border-bottom py-2">
+             <div class="col-1"><input class="form-check-input select-checkbox" type="checkbox" value="" id="flexCheckDefault"></div>
+             <div class="col-9">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aliquid
                  doloremque, minus rem sint tempora, vero reiciendis repudiandae labore maxime aut facilis saepe,
                  nemo provident vel fugit. Et, saepe provident.</div>
-             <div class="col-2">12:32</div>
+             <div class="col-1 text-end">12:32</div>
+             <div class="col-1">
+                 <button type="button" class="btn btn-custom-lgold" data-bs-toggle="button">Elimina</button>
+             </div>
          </div>
      <?php } ?>
  </div>
+ <!-- <div class="col d-none d-md-block">  
+<button type="button" class="btn" data-bs-toggle="button">Già letti</button>  </div> -->
