bb9aa4c9a689a2c9239cf0c94a3e7db0e192ae6b
Giuseppe Marini <giuseppe.marini4@studio.unibo.it>
2024-12-24 14:46:52 +0100

    add modify

--- website/template/template-notifiche.php
+++ website/template/template-notifiche.php
@@ -1,6 +1,6 @@
  <div class="container-fluid text-center my-4">
      <div class="row justify-content-center">
-         <div class="col-12 col-sm-8 col-md-6">
+         <div class="col-12 ol-md-6">
              <div class="d-flex justify-content-center align-items-center gap-3 py-3">
                  <button type="button" class="btn btn-custom-lgold flex-fill mx-1">Seleziona tutto</button>
                  <button type="button" class="btn btn-custom-lgold flex-fill mx-1">Segna come letto</button>
@@ -10,18 +10,18 @@
      </div>
      <?php for ($i = 0; $i < 4; $i++) { ?>
          <div class="row align-items-center border-bottom py-2">
-             <div class="col-12 col-sm-1 text-center">
+             <div class="col-12 col-mm-1 text-center">
                  <input class="form-check-input select-checkbox" type="checkbox" value="" id="flexCheckDefault">
              </div>
-             <div class="col-12 col-sm-8 col-md-9 text-wrap text-break">
+             <div class="col-12 col-md-9 text-wrap text-break">
                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aliquid
                  doloremque, minus rem sint tempora, vero reiciendis repudiandae labore maxime aut facilis saepe,
                  nemo provident vel fugit. Et, saepe provident.
              </div>
-             <div class="col-6 col-sm-2 col-md-1 text-end text-sm-start">
+             <div class="col-6 col-md-1 text-end text-sm-start">
                  12:32
              </div>
-             <div class="col-6 col-sm-1 text-center">
+             <div class="col-6 col-md-1 text-center">
                  <button type="button" class="btn btn-custom-lgold" data-bs-toggle="button">Elimina</button>
              </div>
          </div>
