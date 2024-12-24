ef76c6123d4ef47650edef7167c0c0d54412f839
Giuseppe Marini <giuseppe.marini4@studio.unibo.it>
2024-12-24 09:00:03 +0100

    fix index and template files and add button aggiungi al carrello

--- website/index.php
+++ website/index.php
@@ -1,25 +1,6 @@
- <?php for ($i = 0; $i < 4; $i++) { ?>
-     <div class="container text-center">
-         <div class="row row-cols-1 row-cols-md-3">
-             <div class="col mb-5"><a href="#"><img src="img/temp.jpg" class="img-fluid" alt="Logo" /></a>
-                 <p>ciao</p>
-                 <p>descrizione</p>
-                 <p>prezzo</p>
-                 <p>recensione</p>
-             </div>
-             <div class="col mb-5"><a href="#"><img src="img/temp.jpg" class="img-fluid" alt="Logo" /></a>
-                 <p>ciao</p>
-                 <p>descrizione</p>
-                 <p>prezzo</p>
-                 <p>recensione</p>
-             </div>
-             <div class="col mb-5"><a href="#"><img src="img/temp.jpg" class="img-fluid" alt="Logo" /></a>
-                 <p>ciao</p>
-                 <p>descrizione</p>
-                 <p>prezzo</p>
-                 <p>recensione</p>
-             </div>
-         </div>
-     </div>
+<?php
+require_once("bootstrap.php");
 
- <?php } ?>
+$templateParams["nome"] = "template-index.php";
+
+require("template/base.php");
--- website/template/template-index.php
+++ website/template/template-index.php
@@ -1,6 +1,18 @@
-<?php
-require_once("bootstrap.php");
+ <?php for ($i = 0; $i < 4; $i++) { ?>
+     <div class="container text-center">
+         <div class="row row-cols-1 row-cols-md-3">
+             <?php
+                for ($j = 0; $j < 3; $j++) { ?>
+                 <div class="col mb-5"><a href="#"><img src="img/temp.jpg" class="img-fluid" alt="Logo" /></a>
+                     <header>nome</header>
+                     <p>descrizione</p>
+                     <p>prezzo</p>
+                     <p>recensione</p>
+                     <button type="button" class="btn btn-custom-gold">aggiungi al carrello</button>
+                 </div>
+             <?php
+                } ?>
+         </div>
+     </div>
 
-$templateParams["nome"] = "template-index.php";
-
-require("template/base.php");
+ <?php } ?>
