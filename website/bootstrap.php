3823b952180559f75f56f53aafdc8638f09d2b3c
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-12-25 21:22:52 +0100

    upload_dir more generic

--- website/bootstrap.php
+++ website/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-define("UPLOAD_DIR", "./img/");
+define("UPLOAD_DIR", dirname($_SERVER["SCRIPT_FILENAME"]) . "/img/");
 require_once("utils/functions.php");
 require_once("db/database.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "twdatabase");
-?>
+
