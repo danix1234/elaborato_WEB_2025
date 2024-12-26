26bc67c8d51e53ff32e4a24130cc70c494ddadc6
Botti Bao <botti.bao@studio.unibo.it>
2024-12-26 22:53:11 +0100

    mod sign in, hash password

--- website/sign-in.php
+++ website/sign-in.php
@@ -2,25 +2,24 @@
 require_once("bootstrap.php");
 
 if (!empty($_POST["email"]) && !empty($_POST["password"])) {
-    // sql injection 
-    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $password = $_POST["password"];
 
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $login_result = $dbh->checkLogin($email, $password);
-        if (!empty($login_result)) {
-            registerUser($login_result[0]);
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $user = $dbh->getUserbyEmail($email);
+    if (!empty($user)) {
+        $hash = $user[0]["password"];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) && password_verify($password, $hash)) {
+            registerUser($user[0]);
         } else {
-            $templateParams["erroresignin"] = "Errore, email o password sbagliati.";
+            $templateParams["erroresign"] = "Errore, email o password sbagliati.";
         }
     } else {
-        $templateParams["erroresignin"] = "Errore, email o password sbagliati.";
+        $templateParams["erroresign"] = "Errore, email o password sbagliati.";
     }
 }
 
 if (isLoggedIn()) {
     header("Location: prodotto.php"); //TODO home
-    session_unset();
 } else {
     $templateParams["nome"] = "sign.php";
     $templateParams["tipo"] = "Accedi";
