dea356af85e131cc0e6ea202320aac746993ba7b
Giuseppe Marini <giuseppe.marini4@studio.unibo.it>
2024-12-31 17:44:50 +0100

    modify search template

--- website/template/template-search.php
+++ website/template/template-search.php
@@ -2,9 +2,10 @@
     <div class="row row-cols-1 row-cols-md-3">
         <?php
         foreach ($templateParams["prodotticasuali"] as $prodotto) { ?>
-            <div class="col mb-5"><a href="#"><img src=<?php echo 'img/' . $prodotto["immagine"] ?> class="img-fluid"
-                        alt="Logo" /></a>
-                <header><?php echo $prodotto["nome"] ?></header>
+            <div class="col mb-5"><img src=<?php echo 'img/' . $prodotto["immagine"] ?> class="img-fluid" alt="Logo" />
+                <a href="prodotto.php?productId=<?php echo $prodotto["codProdotto"] ?>" class="row link-custom">
+                    <h1><?php echo $prodotto["nome"] ?></h1>
+                </a>
                 <p><?php echo $prodotto["descrizione"] ?></p>
                 <p><?php echo $prodotto["prezzo"] . '€' ?></p>
                 <p><?php echo 'recensione' ?></p>
