d2c86a6fd2fab0d3f98c0bd9c2c65b38f30dcc7b
Botti Bao <botti.bao@studio.unibo.it>
2025-01-06 17:03:36 +0100

    mv start rating js into product buttons

--- website/js/product-buttons.js
+++ website/js/product-buttons.js
@@ -1,9 +1,9 @@
+// put in cart and buy now buttons
 const quantity = document.getElementsByClassName("button-custom-quantity")[0];
 const productId = new URLSearchParams(window.location.search).get("productId");
 const messageContainer = document.getElementById("message-container");
 const buttonAddItemToCart = document.getElementById("button-add-cart");
 const buttonBuyNow = document.getElementById("button-buy-now");
-
 
 buttonAddItemToCart.addEventListener("click", function () {
     if (isLoggedIn) {
@@ -36,6 +36,41 @@ buttonBuyNow.addEventListener("click", function () {
         window.location.href = `./sign-in.php`;
     }
 });
+
+// star rating
+const stars = document.getElementsByClassName("star");
+const votoRecensione = document.getElementById("votoRecensione");
+
+for (let i = 0; i < stars.length; i++) {
+    
+    stars[i].addEventListener("click", function () {
+        const value = this.getAttribute("data-value");
+        votoRecensione.value = value;
+        updateStars(value);
+    });
+
+    stars[i].addEventListener("mouseover", function () {
+        const value = this.getAttribute("data-value");
+        updateStars(value);
+    });
+
+    stars[i].addEventListener("mouseout", function () {
+        updateStars(votoRecensione.value);
+    });
+}
+
+function updateStars(value) {
+    for (let i = 0; i < stars.length; i++) {
+        if (parseInt(stars[i].getAttribute("data-value")) <= parseInt(value)) {
+            stars[i].classList.add("bi-star-fill");
+            stars[i].classList.remove("bi-star");   
+        } else {
+            stars[i].classList.add("bi-star");
+            stars[i].classList.remove("bi-star-fill");
+        }
+    }
+    
+}
 
 
 
