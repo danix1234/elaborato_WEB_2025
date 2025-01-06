f80f8cd7d1858a5cbada54da1b3c5c644b09503c
Daniele Muffato <daniele.muffato@studio.unibo.it>
2025-01-06 01:53:59 +0100

    stronger floating point input field

--- website/js/float_button.js
+++ website/js/float_button.js
@@ -4,16 +4,36 @@ for (const float_input of document.getElementsByClassName("button-custom-float")
 
     function disallowInvalidChars(element) {
         return function() {
+            console.log(element.value)
+            const cursorStart = element.selectionStart
+            const cursorEnd = element.selectionEnd
+
             let value = element.value.replace(/[^0-9.]/g, '')
             let parts = value.split('.');
             value = parts[0] + (parts.length > 1 ? '.' + parts.slice(1).join('') : '')
-            element.value = value
+
             parts = value.split('.')
-            if (parts.length > 1 && parts[1].length > 2) {
-                element.value = parts[0] + '.' + parts[1].slice(0, 2);
+            value = (parts.length > 1 && parts[1].length > 2) ? (parts[0] + '.' + parts[1].slice(0, 2)) : (value)
+
+            element.value = value
+            element.selectionStart = cursorStart
+            element.selectionEnd = cursorEnd
+        }
+    }
+
+    let validate = function() {
+        return function() {
+            float_input.value = float_input.value.trim()
+            const prev_val = parseFloat(float_input.value)
+            if (!prev_val && float_input.value !== "0" && !float_input.value.startsWith("0.")) {
+                float_input.value = ""
+            } else {
+                // yeah, i am starting to hate js. Why couldn't we just build the web on lua???
+                float_input.value = (Math.trunc(Math.min(max, Math.max(min, prev_val)) * 100) / 100).toFixed(2)
             }
         }
     }
 
+    float_input.addEventListener("change", validate())
     float_input.addEventListener("input", disallowInvalidChars(float_input))
 }
