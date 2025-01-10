8e1ea893aebc20f43b8994621c737a91afcb44f3
Botti Bao <botti.bao@studio.unibo.it>
2025-01-10 16:32:52 +0100

    fix confirm buy deducting prod quantity when operation fails (is there a rollback in php?)

--- website/api/confirm-buy.php
+++ website/api/confirm-buy.php
@@ -17,16 +17,22 @@ if (empty($order) || empty($orderDetails)) {
     die("order already processed");
 }
 
+// check if requested quantity >= remaining
 foreach ($orderDetails as $detail) {
     $quantitaResidua = $dbh->getProduct($detail["codProdotto"])[0]["quantitaResidua"];
     $quantitaFinale = $quantitaResidua - $detail["quantita"];
-    if ($quantitaFinale >= 0) {
-        $dbh->updateProductStock($detail["codProdotto"], $quantitaFinale);
-    } else {
-        //TODO: cancellare ordine? oppure lasciarlo in attesa
+    if ($quantitaFinale < 0) {
         $dbh->modOrderState($orderId, "Deleted", getCurrentUserId());
         die("Errore: quantita' richiesta superiore a quella disponibile!");
-    }
+        
+    } 
 }
+foreach ($orderDetails as $detail) {
+    $quantitaResidua = $dbh->getProduct($detail["codProdotto"])[0]["quantitaResidua"];
+    $quantitaFinale = $quantitaResidua - $detail["quantita"];
+    $dbh->updateProductStock($detail["codProdotto"], $quantitaFinale);
+}
+
+
 $res = $dbh->confirmBuyOrder("Shipping", $orderId, getCurrentUserId());
 ?>
