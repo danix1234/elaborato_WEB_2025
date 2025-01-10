01586f3b980e42161781f2dadfd1bf796d328210
Botti Bao <botti.bao@studio.unibo.it>
2025-01-10 22:08:17 +0100

    add comment in api

--- website/api/update-user-orders.php
+++ website/api/update-user-orders.php
@@ -5,8 +5,12 @@ if (!isLoggedIn()) {
     die('not currently logged in!');
 } 
 
+/*for each order that its state = "shipping" do stuff..
+change the order state into shipped and send the notification*/
+
 $orders = $dbh->getFilteredOrders(getCurrentUserId(), null, "Shipping");
 foreach ($orders as $order){
+    // update the order state, since i removed setting the dataConsegna, we can set it here
     $dbh->updateOrderState("Shipped", $order["codOrdine"], getCurrentUserId());
     $message="";
     $dbh->inserNotification(getCurrentUserId(), "L'ordine #".$order["codOrdine"],"");
