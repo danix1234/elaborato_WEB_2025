773da7f29fb51a4979f5cc550df5cb83d418ac80
Botti Bao <botti.bao@studio.unibo.it>
2025-01-12 00:44:49 +0100

    mod notification message

--- website/api/update-user-orders.php
+++ website/api/update-user-orders.php
@@ -11,7 +11,7 @@ foreach ($orders as $order) {
     $dbh->updateOrdersState(getCurrentUserId());
     $dbh->updateOrderShippedDate(date('Y-m-d H:i:s'), $order["codOrdine"], getCurrentUserId());
     $message = "Ciao " . getCurrentUserName() . ", ";
-    $message.="L'ordine #" . $order["codOrdine"]. "è arrivata al suo indirizzo!";
+    $message.="L'ordine #" . $order["codOrdine"]. " è arrivata!";
     $dbh->inserNotification(getCurrentUserId(), $message, "Arrivo Ordine");
 }
 ?>
